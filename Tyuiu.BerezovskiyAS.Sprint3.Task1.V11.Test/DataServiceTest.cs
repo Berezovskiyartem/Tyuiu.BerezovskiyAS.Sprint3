0af0d79ff4571b8d169e3c194160a9b07b3cd596
Артём Березовский <Tyuiu.BerezovskiyAS@outlook.com>
2025-10-28 17:36:50 +0500

    Task1

---
+++ Tyuiu.BerezovskiyAS.Sprint3.Task1.V11.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.BerezovskiyAS.Sprint3.Task1.V11.Lib;
+namespace Tyuiu.BerezovskiyAS.Sprint3.Task1.V11.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultiplySeries()
+        {
+            DataService ds = new DataService();
+            double value = 0.75;
+            int startValue = 1;
+            int stopValue = 14;
+
+            double res = ds.GetMultiplySeries(value, startValue, stopValue);
+            double wait = 0.005;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
