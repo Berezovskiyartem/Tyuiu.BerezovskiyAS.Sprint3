de56063862538a16f101cad6373b3675cd18605a
Артём Березовский <Tyuiu.BerezovskiyAS@outlook.com>
2025-10-28 17:04:56 +0500

    Add project files.

---
+++ Tyuiu.BerezovskiyAS.Sprint3.Task0.V2.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.BerezovskiyAS.Sprint3.Task0.V2.Lib
+{
+    public class DataService : ISprint3Task0V2
+    {
+        public double GetMultiplySeries(int startValue, int stopValue)
+        {
+            double result = 1;
+            for (int i = startValue; i <= stopValue; i++)
+            {
+                result *= Math.Pow((i / (Math.Pow(Math.Sin(1), -7))), -2);
+            }
+            return Math.Round(result, 3);
+        }
+    }
+}
---
+++ Tyuiu.BerezovskiyAS.Sprint3.Task0.V2.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.BerezovskiyAS.Sprint3.Task0.V2.Lib;
+namespace Tyuiu.BerezovskiyAS.Sprint3.Task0.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultiplySeries()
+        {
+            DataService ds = new DataService();
+            int start1 = 1;
+            int finish2 = 9;
+            var res = ds.GetMultiplySeries(start1, finish2);
+            double wait = 0.021;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
